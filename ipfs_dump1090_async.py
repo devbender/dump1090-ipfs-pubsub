d1cdf25fc29547cc1147813ce51d8e562518beff
Juan Benitez <juan.a.benitez@gmail.com>
2022-03-01 01:03:06 -0400

    async implementation

---
+++ ipfs_dump1090_async.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python3
+
+###############################################################################
+# IPFS PUBSUB DUMP1090 v0.1 
+# Copyright (C) 2022  Juan Benitez
+# Distributed under GPLv3
+###############################################################################
+import asyncio, logging,json
+from time import time
+from collections import OrderedDict
+
+tasks = []
+localCache = {}
+
+###############################################################################
+# SBS1 MSG FIELDS
+###############################################################################
+msgtype_field = 1
+icao_field = 4
+date_field = 6
+time_field = 7
+
+callsign_field = 10
+
+alt_field = 11
+lat_field = 14
+lon_field = 15
+
+gspd_field = 12
+trk_field = 13
+vrt_field = 16
+
+gndf = 21
+
+###############################################################################
+# Process SBS1 line
+###############################################################################
+def SBS1toDict( line ):
+   SBS1 = line.split(",")
+   msgtype = SBS1[msgtype_field]
+   icao = SBS1[icao_field]
+   
+   if( SBS1[gndf] == "" ):    ongnd = 0
+   elif (SBS1[gndf] == "-1"): ongnd = 1
+   else: ongnd = 0
+     
+   if msgtype == '1':
+      return { 'id': int(msgtype), 
+               'icao': icao, 
+               'csg': SBS1[callsign_field].rstrip(), 
+               'ts': int(time()) }
+      
+   elif msgtype == '2': 
+      return { 'id': int(msgtype), 
+               'icao': icao,  
+               'alt': SBS1[alt_field],
+               'lat': SBS1[lat_field], 
+               'lon': SBS1[lon_field],
+               'spd': SBS1[gspd_field],
+               'trk': SBS1[trk_field],
+               'gnf': ongnd,
+               'ts': int( time() )
+               }
+   
+   elif msgtype == '3':
+      if SBS1[lat_field] != '' and SBS1[lon_field] != '':
+         return { 'id': int(msgtype), 
+                  'icao': icao, 
+                  'alt': int(SBS1[alt_field]),
+                  'lat': float(SBS1[lat_field]), 
+                  'lon': float(SBS1[lon_field]), 
+                  'gnf': ongnd,
+                  'ts': int( time() )
+                  }
+
+   elif msgtype == '4':
+      return { 'id': int(msgtype), 
+               'icao': icao, 
+               'spd': int(SBS1[gspd_field]),
+               'trk': int(SBS1[trk_field]),
+               'vrt': int(SBS1[vrt_field]), 
+               'ts': int( time() )
+               }
+
+###############################################################################
+# UPDATE LOCAL CACHE
+###############################################################################
+def updateLocalCache( data ):
+
+   msgid = data['id']
+   icao = data['icao']
+
+   # AIRCRAFT in CACHE =============================================
+   if icao in localCache:
+
+      # If in local cache update data
+      if msgid == 1:
+         logging.debug(f"[UPDATED][CALLSIGN] {icao}")
+         localCache[icao]['csg'] = data['csg']
+
+         # call sign msg does not trigger an update
+
+      elif msgid == 2:
+         logging.debug(f"[UPDATED][G-LOCATION] {icao}")
+         localCache[icao]['alt'] = 0
+         localCache[icao]['lat'] = data['lat']
+         localCache[icao]['lon'] = data['lon']
+         localCache[icao]['gnf'] = data['gnf']
+
+         localCache[icao]['new'] = 1
+               
+      elif msgid == 3:
+         logging.debug(f"[UPDATED][LOCATION] {icao}")
+         localCache[icao]['alt'] = data['alt']
+         localCache[icao]['lat'] = data['lat']
+         localCache[icao]['lon'] = data['lon']
+         localCache[icao]['gnf'] = data['gnf']
+
+         localCache[icao]['new'] = 1
+
+      elif msgid == 4:
+         logging.debug(f"[UPDATED][VECTOR] {icao}")
+         localCache[icao]['spd'] = data['spd']
+         localCache[icao]['trk'] = data['trk']
+         localCache[icao]['vrt'] = data['vrt']
+
+         localCache[icao]['new'] = 1
+      
+      else: pass
+
+      # Common fields
+      localCache[icao]['ts'] = data['ts']
+      
+
+   # NEW AIRCRAFT ================================================
+   else:
+      localCache[icao] = {'csg': '', 
+                          'alt': '', 
+                          'lat': '', 
+                          'lon': '', 
+                          'gnf': '', 
+                          'spd': '', 
+                          'trk': '', 
+                          'vrt': '' }
+     
+      if msgid == 1:
+         logging.debug(f"[NEW][CALLSIGN] {icao}")
+         localCache[icao] = { 'csg': data['csg'] }
+
+      if msgid == 3:
+         logging.debug(f"[NEW][LOCATION] {icao}")
+         localCache[icao] = { 'alt': data['alt'], 
+                              'lat': data['lat'], 
+                              'lon': data['lon'] }
+      if msgid == 4:
+         logging.debug(f"[NEW][VECTOR] {icao}")
+         localCache[icao] = { 'spd': data['spd'], 
+                              'trk': data['trk'], 
+                              'vrt': data['vrt'] }
+
+      # Common fields      
+      localCache[icao]['new'] = 1
+      localCache[icao]['icao'] = str(data['icao'])
+      localCache[icao]['ts'] = data['ts']
+
+
+###############################################################################
+# Get dump1090 SBS1 data
+###############################################################################
+async def getSBS1DataTask( dump1090Host='localhost', dump1090Port=30003, frameSizeKb=64 ):
+   logging.info("TASK: [getSBS1DataTask] Started")
+    
+   reader, writer = await asyncio.open_connection( dump1090Host, dump1090Port)
+
+   while True:
+      data = await reader.read( frameSizeKb*1024 )
+      data = data.decode('utf-8').splitlines()        
+        
+      for line in data:
+         if line != '':
+            try: val = SBS1toDict(line)
+            except: pass
+            if val != None:                   
+               updateLocalCache(val)
+
+
+###############################################################################
+# DATA EXPORT
+###############################################################################
+async def exportDataTask( callback=None):
+   logging.info("TASK: [exportDataTask] Started")
+
+   export_key_order = ['icao', 'csg', 'ts', 'alt', 'lat', 'lon', 'spd', 'trk', 'vrt', 'gnf']
+
+   while True:
+      await asyncio.sleep(1)
+        
+      for icao in list(localCache):
+         if ('lat' in localCache[icao]) and \
+            ('lon' in localCache[icao]) and \
+            ( localCache[icao]['new'] ):
+                  
+            dataOUT = dict( OrderedDict((k, localCache[icao].get(k)) for k in export_key_order) )            
+            if callback is not None: 
+               callback( dataOUT )
+
+            localCache[icao]['new'] = 0
+
+      with open('dump1090.json', 'w') as outfile:
+         json.dump(localCache, outfile, indent=4)
+
+
+###############################################################################
+# DATA CLEANUP
+###############################################################################
+async def localCleanupTask(exportMetadata, cleanEveryXsecs=10, expireEveryYsecs=60):
+
+   logging.info("TASK: [localCleanupTask] Started")
+   
+   while True:
+      await asyncio.sleep(cleanEveryXsecs)
+   
+      for icao in list(localCache):
+
+         # Remove aircraft from local cache if no new data in last X seconds
+         if (time() - localCache[icao]['ts'] ) > expireEveryYsecs:
+            localCache.pop(icao, None)
+            logging.debug(f"[REMOVED] {icao}")
+      
+         else: pass
+
+      exportMetadata()
+
+###############################################################################
+# ASYNC MAIN
+###############################################################################
+async def main( exportCallback=None, metadataCallback=None, loglevel='INFO' ):
+   
+   # Setup logging
+   numeric_level = getattr(logging, loglevel.upper())
+   fmt = '[%(levelname)s] %(asctime)s - %(message)s'
+   logging.basicConfig(level=numeric_level, format=fmt)
+
+   logging.info("Starting tasks...")
+   
+   task1 = asyncio.create_task( getSBS1DataTask('10.0.0.100', 30003) )
+   task2 = asyncio.create_task( exportDataTask(exportCallback) )
+   task3 = asyncio.create_task( localCleanupTask(metadataCallback, 10, 60) )
+
+   tasks.append(task1)
+   tasks.append(task2)
+   tasks.append(task3)
+   
+   logging.info("*** RUNNING ***")
+    
+   while True: await asyncio.sleep(1)
+
+###############################################################################
+# RUN
+###############################################################################
+def run( exportCallback=None, metadataCallback=None, loglevel='INFO', metadata=None ):
+   
+   try: asyncio.run( main(exportCallback, metadataCallback, loglevel) )
+   
+   except KeyboardInterrupt:
+
+      logging.info("KeyboardInterrupt")
+      logging.info("Stopping tasks...")
+
+      # Cancel all tasks
+      for task in tasks:
+         task.cancel()
+
+      # Clear JSON file
+      with open('dump1090.json', 'w') as outfile:
+         json.dump({}, outfile, indent=4)
+
+      logging.info("*** TERMINATED ***")
+
+###############################################################################
+# MAIN
+###############################################################################
+if __name__ == "__main__":
+   run( lambda x: print(x), 'INFO' )
--- ipfs_pub_dump1090.py
+++ ipfs_pub_dump1090.py
@@ -5,22 +5,46 @@
 # Copyright (C) 2022  Juan Benitez
 # Distributed under GPLv3
 ###############################################################################
-import json
 import ipfs_pubsub as ipfs
-import ipfs_dump1090 as dump1090
-from collections import OrderedDict
+import ipfs_dump1090_async as dump1090
+from uuid import uuid4
+from json import dump, load as loadjson
 
-TOPICS = []
+data = 'ADSB'
+airports = ['SDQ', 'JQB']
 
-def onData( data ):
-    #print( data )    
-    for topic in TOPICS: 
-        ipfs.publish( topic, data )
+MY_RX_LOCATION = [18.465, -69.942]
+DATA_FORMAT = 'ndjson'
 
-dump1090.run( host = 'localhost',
-              port = 30003,
-              callback = onData, 
-              data_out_secs = 1,
-              aircraft_stale = 5,
-              aircraft_timeout = 30,
-              loglevel = 'INFO' )
+PEER_PUBSUB_CHANNEL = uuid4().hex
+METADATA_PUBSUB_CHANNELS = []
+
+for airport in airports:
+    refPoint = data + '-' + airport
+    METADATA_PUBSUB_CHANNELS.append( refPoint )
+
+meta = {'id': PEER_PUBSUB_CHANNEL,        
+        'format': DATA_FORMAT, 
+        'location': MY_RX_LOCATION,
+        'tracking': 0 }
+
+# PUBLISH ADS-B DATA
+def onData( data ):    
+    ipfs.publish( PEER_PUBSUB_CHANNEL, data )
+
+# PUBLISH METADATA
+def metaData():
+
+    # Check how many aircrafts we are tracking
+    with open('dump1090.json', 'r') as f:
+        data = loadjson(f)
+        meta['tracking'] = len(data)
+
+    # Publish metadata
+    for channel in METADATA_PUBSUB_CHANNELS:
+        ipfs.publish( channel, meta )
+
+###############################################################################
+# RUN MAIN
+###############################################################################
+dump1090.run(onData, metaData)
